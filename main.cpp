fbe8c23ab4d8a555ba3241a3695e98b4af03d43d
Kaja <80481808+GreekYogurtPowder@users.noreply.github.com>
2021-03-11 19:31:30 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "instancja.h"
+#include "RandomNumberGenerator.h"
+using namespace std;
+
+
+int main()
+{
+	instancja inst = instancja();
+	inst.wypiszTabele();
+	inst.calculate();
+
+
+    return 0;
+}
---
+++ zadanie.h
@@ -0,0 +1,14 @@
+#ifndef ZADANIE_H
+#define ZADANIE_H
+
+class zadanie
+{
+	public:
+		long r, p, q, j; //czasy i nr zadania
+		
+		zadanie();
+		zadanie(long j, long r, long p, long q);
+	protected:
+};
+
+#endif
